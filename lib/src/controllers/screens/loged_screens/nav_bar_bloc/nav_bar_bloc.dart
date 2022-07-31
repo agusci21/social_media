83aed2514c50f09b991d678aa9b1e88fedc5c65b
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2022-07-31 11:41:01 -0300

    Nav bar y screen de usuario

--- lib/src/controllers/screens/loged_screens/nav_bar_bloc/nav_bar_bloc.dart
+++ lib/src/controllers/screens/loged_screens/nav_bar_bloc/nav_bar_bloc.dart
@@ -2,6 +2,7 @@
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
 
 part 'nav_bar_event.dart';
 part 'nav_bar_state.dart';
@@ -10,6 +11,9 @@ class NavBarBloc extends Bloc<NavBarEvent, NavBarState> {
   NavBarBloc() : super(const NavBarState(0)) {
     on<SelectOption>((event, emit) {
       emit(NavBarState(event.selectedOption));
+      controller.animateToPage(state.selectedOption,
+          duration: const Duration(milliseconds: 200), curve: Curves.easeIn);
     });
   }
+  final controller = PageController();
 }
--- lib/src/ui/screens/loged_screens/home_screen/home_screen.dart
+++ lib/src/ui/screens/loged_screens/home_screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_media/src/controllers/screens/loged_screens/nav_bar_bloc/nav_bar_bloc.dart';
 import 'package:social_media/src/ui/screens/loged_screens/home_screen/components/home_nav_bar.dart';
+import 'package:social_media/src/ui/screens/loged_screens/home_screen/components/profile_section/main_profile_section.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,6 +14,17 @@ class HomeScreen extends StatelessWidget {
       child: BlocBuilder<NavBarBloc, NavBarState>(
         builder: (context, state) {
           return Scaffold(
+            body: PageView(
+              controller: context.read<NavBarBloc>().controller,
+              children: [
+                for (int i = 0; i < 2; i++)
+                  ListView.builder(
+                    itemBuilder: (context, index) =>
+                        Text('${i + 1} x $index = ${index * (i + 1)}'),
+                  ),
+                const MainProfileSection()
+              ],
+            ),
             bottomNavigationBar: HomeNavBar(
                 onTap: (value) =>
                     context.read<NavBarBloc>().add(SelectOption(value)),
