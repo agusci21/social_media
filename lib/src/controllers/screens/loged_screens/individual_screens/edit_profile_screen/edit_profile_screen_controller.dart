94defcf68cf82067877fe53ecb3f9330a1dacb17
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2022-08-02 14:21:21 -0300

    Modificacion del usuario exitosa

--- lib/src/controllers/screens/loged_screens/individual_screens/edit_profile_screen/edit_profile_screen_controller.dart
+++ lib/src/controllers/screens/loged_screens/individual_screens/edit_profile_screen/edit_profile_screen_controller.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
 import 'package:social_media/src/controllers/general_blocs/user/user_bloc.dart';
@@ -23,5 +26,16 @@ abstract class EditProfileScreenController {
     });
     await http.put(Uri.parse('${Enviroment.usersUrl}/modify'),
         headers: Enviroment.headers, body: body);
+    showDialog(
+        context: context,
+        builder: (BuildContext context) => const AlertDialog(
+              content: Center(
+                heightFactor: 0.5,
+                widthFactor: 0.5,
+                child: Text('Usuario modificado, \nInicie sesión Nuevamente'),
+              ),
+            ));
+    await Future.delayed(const Duration(seconds: 3));
+    closeSesion(context);
   }
 }
